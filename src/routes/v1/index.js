6cdf5a283c81762d33ddf00294e52193d75038e6
Saksham Singhal <26411548+Duskmate@users.noreply.github.com>
2024-05-01 15:48:52 +0530

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const videoController = require("../../controllers/video.controller")
 
 const router = express.Router();
 
-router.get("/videos", videoController.getVideos)
+router.get("/videos", function (req, res, next) {
+  res.send("data");
+})
 
 module.exports = router;
