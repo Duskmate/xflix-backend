fc458cb338dbf36dcc2232b43992c7ac03b021bd
Saksham Singhal <26411548+Duskmate@users.noreply.github.com>
2024-05-01 15:47:15 +0530

    Update video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,8 +1,1 @@
-const { Video } = require("../models")
-
-const getVideos = async(req, res) => {
-    let videos = await Video.find({})
-    res.send(videos)
-}
-
-module.exports = {getVideos};
+module.exports = {};
